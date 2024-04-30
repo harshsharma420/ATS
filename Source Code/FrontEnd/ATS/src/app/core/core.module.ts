ea984bbd20c62175960d912b8e77e0d26eaeba0e
Harsh Sharma <sharmaharsh555@gmail.com>
2024-04-30 20:34:25 +0530

    An atempt at lazyloading will continue tomorrow

--- Source Code/FrontEnd/ATS/src/app/core/core.module.ts
+++ Source Code/FrontEnd/ATS/src/app/core/core.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+// import { HeaderComponent } from './components/header/header.component';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: HeaderComponent
+  }
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
+ 
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes),
+  ],
+  exports: [RouterModule]
 })
 export class CoreModule { }
