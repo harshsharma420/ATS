3e0e4c5b79331f89bb90258fd042918ae97a8c36
Harsh Sharma <sharmaharsh555@gmail.com>
2024-05-07 18:00:03 +0530

    Added resumes route

--- Source Code/BackEnd/server.js
+++ Source Code/BackEnd/server.js
@@ -3,14 +3,16 @@ require('dotenv').config();
 const app = express();
 
 app.get('/', (req, res) => {
-    console.log('Here');
+    console.log('This is a server for the ATS project');
     // res.render('index');
     // res.download('server.js');
     // res.json({ message: "Error"});
 });
 
 const userRouter = require('./routes/users');
+const resumesRouter = require('./routes/resumes');
 
-app.use('/users' , userRouter);
+app.use('/users', userRouter);
+app.use('/resumes', resumesRouter);
 
 app.listen(process.env.PORT);
