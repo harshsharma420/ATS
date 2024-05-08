c5d34683a24a05359d2486730ccdffb3b77175bb
Harsh Sharma <sharmaharsh555@gmail.com>
2024-05-08 12:34:46 +0530

    Implemented Multer

--- Source Code/BackEnd/routes/resumes.js
+++ Source Code/BackEnd/routes/resumes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const upload = multer({ dest: '../userUploadedResumes/'})
 
 router.post('/newResume', (req, res) => {
     res.send("file recieved");
