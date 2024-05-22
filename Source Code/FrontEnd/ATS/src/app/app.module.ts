04e16cb87fcbdd66fcc1811ba8065819fc4a62d8
Harsh Sharma <sharmaharsh555@gmail.com>
2024-05-22 18:51:04 +0530

    Learned how to actually implement lazyloading

--- Source Code/FrontEnd/ATS/src/app/app.module.ts
+++ Source Code/FrontEnd/ATS/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './core/components/header/header.component';
+// import { HeaderComponent } from './core/components/header/header.component';
+// import { CoreModule } from './core/core.module';
 
 
 @NgModule({
