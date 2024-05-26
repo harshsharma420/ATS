035f14aa44c1d939748acc653e0af19a460a7036
Harsh Sharma <sharmaharsh555@gmail.com>
2024-05-26 17:26:36 +0530

    added new Component

--- Source Code/FrontEnd/ATS/src/app/features/user-home-screen/user-home-screen.component.ts
+++ Source Code/FrontEnd/ATS/src/app/features/user-home-screen/user-home-screen.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { TitleComponent } from 'src/app/shared/UI components/title/title.component';
 import { SubtitleComponent } from 'src/app/shared/UI components/subtitle/subtitle.component';
 import { UserUploadButtonComponent } from 'src/app/shared/UI components/user-upload-button/user-upload-button.component';
+import { OrComponent } from 'src/app/shared/UI components/or/or.component';
 
 @Component({
   selector: 'app-user-home-screen',
   standalone: true,
-  imports: [CommonModule , TitleComponent , SubtitleComponent , UserUploadButtonComponent],
+  imports: [CommonModule , TitleComponent , SubtitleComponent , UserUploadButtonComponent , OrComponent],
   templateUrl: './user-home-screen.component.html',
   styleUrls: ['./user-home-screen.component.scss']
 })
